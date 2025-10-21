915fd22c6cfc33ac97f19c0315907711397f8817
wszdb <zzdogtxt@gmail.com>
2025-10-21 22:32:22 +0800

    upload dist

---
+++ js/dist/forum.js
@@ -0,0 +1,2 @@
+(()=>{var e={n:o=>{var r=o&&o.__esModule?()=>o.default:()=>o;return e.d(r,{a:r}),r},d:(o,r)=>{for(var t in r)e.o(r,t)&&!e.o(o,t)&&Object.defineProperty(o,t,{enumerable:!0,get:r[t]})},o:(e,o)=>Object.prototype.hasOwnProperty.call(e,o)};(()=>{"use strict";const o=flarum.core.compat["forum/app"];e.n(o)().initializers.add("wszdb/flarum-homefilter",function(){console.log("[HomeFilter] Extension loaded")})})(),module.exports={}})();
+//# sourceMappingURL=forum.js.map
